dbae0edca17af43e60e127fe3e76c0c6b8c25ee4
thao <thaobee2407@gmail.com>
2024-03-13 22:58:20 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// var bien='Heloo';
+// alert(bien);
+// console.log(bien);
+
+// confirm('Xac nhan ban du tuoi');
+
+// prompt('Xac nhan du tuoi');
+
+// setInterval( function() {
+//     alert('Hello')
+// },1000)
+
+var a='abcb'
+var b='ebca'
+
+if(a>b)
+{
+    console.log(a)
+}
+else{
+    console.log(b)
+}
